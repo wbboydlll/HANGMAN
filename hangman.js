2f60b23183ed4af4f064c2e7c754505768aa30ab
wbboydlll <wbboydlll@gmail.com>
2025-10-15 10:45:36 -0400

    finished the .js at 10:45am

---
+++ hangman.js
@@ -0,0 +1,4 @@
+var POSSIBLE_WORDS = ["obdurate", "verisimiltude", "defenestrate", "obsequious", "dissonant", "toady", "indempotent"];
+
+var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS[randomIndex];
