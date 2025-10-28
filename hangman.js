efb44c5bb31d99ee4c38249220c955ec5faab4d2
wbboydlll <wbboydlll@gmail.com>
2025-10-28 19:32:53 -0400

    took out emojis didnt like how they looked after the fact

--- hangman.js
+++ hangman.js
@@ -44,8 +44,8 @@ function updatePage() {
     image.src = "images/hangman" + guess_count + ".gif";
 }
 
-document.getElementById("guesses").textContent = "🎉 Congratulations! You guessed the word!";
-document.getElementById("guesses").textContent = `💀 You lost! The word was '${selectedWord}'.`;
+document.getElementById("guesses").textContent = "Congratulations! You guessed the word!";
+document.getElementById("guesses").textContent = `You lost! The word was '${selectedWord}'.`;
 
 if (gameOver) {
     document.getElementById("guesses").textContent = "The game is over. Please start a new game.";
